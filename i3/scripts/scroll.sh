f86ab0d6f1c8c9e12ebb8f40b1a565324e53800d
Emile Segers <emile.segers8@gmail.com>
2020-01-04 20:43:22 +0100

    Deleted files adden

--- i3/scripts/scroll.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-id=$(xinput --list | grep "ALPS" | grep -o "=[0-9]*" | grep -o "[0-9]*")
-
-$(xinput --set-prop $id "Synaptics Two-Finger Scrolling" 1 1)
-$(xinput --set-prop $id "Synaptics Scrolling Distance" -26 -26)
--- i3/scripts/volume.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-percentage=$(amixer get Master | egrep -o "\[[0-9]*\%\]" | egrep -o "[0-9]*\%")
-
-echo $percentage
